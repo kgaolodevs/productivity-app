f652afd56a0fa590105b1c4004e83222055c88a8
kgaolodevs <kgaolodev@gmail.com>
2020-08-11 09:46:44 +0200

    Added background color to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/font-awesome/css/font-awesome.css";
+import "../src/css/main.css";
 
 const App = () => {
   return <h1>Productivity App</h1>;
